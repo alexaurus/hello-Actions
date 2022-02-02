2648910ed4e9d684b20798e3eb020ad3add2f59b
alexaurus <a.cole.sw@gmail.com>
2022-02-01 21:26:30 -0600

    init

---
+++ hello-Actions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hello_Actions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
